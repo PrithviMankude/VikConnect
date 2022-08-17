5bae747a69e88c3bf6838bfa2fdde551536f45fb
PrithviMankude <prithvi.mramesh@gmail.com>
2022-08-17 16:10:20 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API running..');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
